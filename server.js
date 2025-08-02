d6a5cc2c6a71aa3cbe08e4eb7e692c2cf4524d38
hnin-ei-phyu <nanghnineiphyu2001@gmail.com>
2025-08-02 16:56:42 +0700

    Updated login

--- server.js
+++ server.js
@@ -1,22 +1,27 @@
 import express from 'express'
 import cors from 'cors'
-import 'dotenv/config'
+import dotenv from 'dotenv'
 import connectDB from './config/mongodb.js'
 import connectCloudinary from './config/cloudinary.js'
+import adminRouter from './routes/adminRoute.js'
 
 //app config
+dotenv.config()
 const app = express()
 const port = process.env.PORT || 4000
 connectDB()
 connectCloudinary()
 
 //middlewares
-app.use(express.json())
+app.use(express.json()) //for  application/json
+app.use(express.urlencoded({extended: true})) //for form submissions
 app.use(cors())
 
 
 
 //api endpoints
+app.use('/api/admin', adminRouter)
+//localhost:4000/api/admin
 
 app.get('/', (req,res) => {
     res.send('API IS WORKING!')
